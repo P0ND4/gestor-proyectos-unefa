7f60097e453442a7aae4e546eb3b3e2236e749f8
FyZeroMaster <yonehernandez27@gmail.com>
2025-03-22 21:19:52 -0400

    inicio-header

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { initFlowbite } from 'flowbite';
+import { HeaderComponent } from './shared/header/header.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'frontend';
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
 }
